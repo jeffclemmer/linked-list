a76324bad3c4794e6fbc6342c4f5060c62dd8175
jeff clemmer <jeff.clemmer@terwil.com>
2022-09-06 17:32:25 -0700

    add: README and .gitignore. cleanup: main.cpp.

---
+++ README.md
@@ -0,0 +1,16 @@
+## Simple Singly Linked List In C++
+
+This tiny project defines a class that handles push() and shift() operations for a simple fifo style queue.
+
+## Building
+
+Clone the repo, cd into the directory, and
+```
+make
+```
+
+## Running
+
+```
+./list
+```
--- main.cpp
+++ main.cpp
@@ -1,91 +1,98 @@
 #include <iostream>
 
+// define a struct to hold each list node
 struct node {
-	int data;
-	node *next = nullptr;
+  int data;
+  node *next = nullptr;
 };
 
+// a class that handles all the linked operations
 class list {
 private:
-	node *start = nullptr;
-	node *head = nullptr;
-	int _length = 0;
+  node *start = nullptr;
+  node *head = nullptr;
+  int _length = 0;
 public:
 
-	// delete all nodes on class destruction
-	~list() {
-		
-		int deleted = 0;
-		while (start != nullptr) {
-			node *next = start->next;
-			delete start;
-			deleted++;
-			if (next != nullptr)
-				start = next;
-			else
-				break;
-		}
-		
-		std::cout << "deleted " << deleted << " nodes" << std::endl;
-		
-	}
-	
-	
-	int length() {
-		return _length;
-	};
-	
-	
-	void push(const int &data) {
-		std::cout << "pushing " << data << std::endl;
-		node *newNode = new node;
-		newNode->data = data;
-		
-		if (start == nullptr) {
-			start = newNode;
-			head = start;
-		} else {
-			head->next = newNode;
-			head = head->next;
-		}
-		
-		_length++;
-		
-	};
-	
-	
-	int shift() {
-		if (start != nullptr) {
-			int ret = start->data;
-			
-			node * next = start->next;
-			delete start;
-			
-			start = next;
-			
-			_length--;
-			
-			return ret;
-		}
-		
-		return 0;
-	};
-	
+  // delete all nodes on class destruction
+  ~list() {
+    
+    int deleted = 0;
+    while (start != nullptr) {
+      node *next = start->next;
+      delete start;
+      deleted++;
+      if (next != nullptr)
+        start = next;
+      else
+        break;
+    }
+    
+    std::cout << "deleted " << deleted << " nodes" << std::endl;
+    
+  }
+  
+  
+  int length() {
+    return _length;
+  };
+  
+  
+  // push a node on to the back of the list 
+  void push(const int &data) {
+    std::cout << "pushing " << data << std::endl;
+    node *newNode = new node;
+    newNode->data = data;
+    
+    if (start == nullptr) {
+      start = newNode;
+      head = start;
+    } else {
+      head->next = newNode;
+      head = head->next;
+    }
+    
+    _length++;
+    
+  };
+  
+  
+  // shift a node from the front of the list and delete the entry
+  int shift() {
+    if (start != nullptr) {
+      int ret = start->data;
+      
+      node * next = start->next;
+      delete start;
+      
+      start = next;
+      
+      _length--;
+      
+      return ret;
+    }
+    
+    return 0;
+  };
+
 };
 
 
+// do some quick testing in our main function
 int main(int argc, char **argv) {
-	list list;
-	
-	list.push(123);
-	list.push(678);
-	
-	std::cout << "list length: " << list.length() << std::endl;
-	
-	std::cout << "shifted: " << list.shift() << std::endl;
-	std::cout << "shifted: " << list.shift() << std::endl;
-	
-	std::cout << "list length: " << list.length() << std::endl;
-	
-	return 0;
+  
+  // create a list
+  list list;
+  
+  list.push(123);
+  list.push(678);
+  
+  std::cout << "list length: " << list.length() << std::endl;
+  
+  std::cout << "shifted: " << list.shift() << std::endl;
+  std::cout << "shifted: " << list.shift() << std::endl;
+  
+  std::cout << "list length: " << list.length() << std::endl;
+  
+  return 0;
 }
